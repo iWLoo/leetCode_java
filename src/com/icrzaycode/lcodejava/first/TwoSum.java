04dde63ed99944e7f50be577d02bbc9dca3c9e3a
ybh <ybh1547@163.com>
2019-05-29 00:18:59 +0800

    配置了公司的mac环境和家里的win环境，再补充第二道题的文字，明天正式开始，刷刷刷。

--- src/com/icrzaycode/lcodejava/first/TwoSum.java
+++ src/com/icrzaycode/lcodejava/first/TwoSum.java
@@ -8,7 +8,7 @@ import java.util.Map;
  */
 public class TwoSum {
     /**
-     * 两数之和_0001
+     * 两数之和_0001_E
      *
      * 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
      * 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用
@@ -18,17 +18,17 @@ public class TwoSum {
      * array[0]+array[1]=9
      * 返回[0,1]
      */
-    private static Integer[] getTwoSumIndex(Integer[] array,int target){
-        Integer[] indexArray = new Integer[2];
+    private static int[] getTwoSumIndex(int[] nums,int target){
+        int[] indexArray = new int[2];
         //准备数据，将所有的数值都放到map中。
         Map<Integer,Integer> valueIndex = new HashMap<>(16);
-        for(int i=0;i<array.length;i++){
-            valueIndex.put(array[i],i);
+        for(int i=0;i<nums.length;i++){
+            valueIndex.put(nums[i],i);
         }
         //遍历数组，看看是否有符合的值
-        for(int i=0;i<array.length;i++){
+        for(int i=0;i<nums.length;i++){
             //当前的值
-            int nowValue = array[i];
+            int nowValue = nums[i];
             //距离目标值剩下的值
             int otherValue = target - nowValue;
             if(valueIndex.containsKey(otherValue) && !valueIndex.get(otherValue).equals(i)){
@@ -42,9 +42,9 @@ public class TwoSum {
     }
 
     public static void main(String[] args) {
-        Integer[] array = new Integer[]{2,5,11,15};
+        int[] array = new int[]{2,5,11,15};
         int target = 16;
-        Integer[] resultIndex = getTwoSumIndex(array,target);
+        int[] resultIndex = getTwoSumIndex(array,target);
         System.out.println(resultIndex[0]);
         System.out.println(resultIndex[1]);
     }
